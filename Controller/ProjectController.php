8ee9d18b5ffc737d58abacd04265b32de0b386c3
dawidbros1 <dawidbros1@wp.pl>
2022-02-15 09:38:38 +0100

    ProjectController [ get | create ] Action

---
+++ Controller/ProjectController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Controller;
+
+use Model\Project;
+use Rules\ProjectRules;
+
+class ProjectController extends Controller
+{
+   public function __construct($action)
+   {
+      parent::__construct($action);
+      $this->rules = new ProjectRules();
+      $this->project = new Project();
+   }
+
+   public function getAction()
+   {
+      $user_id = $this->getData(['user_id'])->user_id;
+      $projects = $this->project->get($user_id);
+      $this->response->success($projects);
+   }
+
+   public function createAction()
+   {
+      $data = $this->getData(['name', 'description'], true);
+
+      [$validateStatus, $validateMessages] = $this->validate((array) $data, $this->rules);
+
+      if ($validateStatus) {
+         $this->project->create((array) $data);
+         $this->response->success();
+      }
+
+      $this->response->error(403, $validateMessages);
+   }
+
+   // public function updateAction()
+   // {
+   // }
+
+   // public function deleteAction()
+   // {
+   // }
+}
