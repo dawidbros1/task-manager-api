ab0b2e4dce4ef22220fd02bb2c174b1eac566b8e
dawidbros1 <dawidbros1@wp.pl>
2022-02-15 09:39:40 +0100

    addded ProjectRules

---
+++ Rules/ProjectRules.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rules;
+
+use Model\General\Rules;
+
+class ProjectRules extends Rules
+{
+   public function __construct()
+   {
+      $this->rules();
+      $this->messages();
+   }
+
+   public function rules()
+   {
+      $this->createRules('name', ['min' => 3, "max" => 64, 'specialCharacters' => true]);
+      $this->createRules('description', ['max' => 2550]);
+   }
+
+   public function messages()
+   {
+      $this->createMessages('name', [
+         'between' => "Nazwa projektu powinna zawierać od " . $this->value('name.min') . " do " . $this->value('name.max') . " znaków",
+         'specialCharacters' => "Nazwa projektu nie może zawierać znaków specjalnych",
+      ]);
+
+      $this->createMessages('description', [
+         'max' => "Opis nie może zawierać więcej niż " . $this->value('description.max') . " znaków",
+      ]);
+   }
+}
